97893fdea875bfc5830f12cf7fab22224d87485e
Chandler EuBank <Chan@Chandlers-MacBook-Pro.local>
2020-11-01 16:08:57 -0600

    finished part one of the first exercise

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
-  image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  photosTitle = 'Perfectly Positioned Pics';
+  image1 = 'https://peopledotcom.files.wordpress.com/2017/08/shoppingapp.jpg?w=768';
+  image2 = 'https://i1.wp.com/nerdbastards.com/wp-content/uploads/2013/04/Kikis_Delivery_Service_full_937028.jpg';
+  image3 = 'https://dontgetserious.com/wp-content/uploads/2018/07/travel-gadgets.jpg';
 
   constructor() { }
 
